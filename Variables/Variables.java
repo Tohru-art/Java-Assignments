cf941ffb1fba7760e077155527d749dedbba4487
Tohru-art <willgarlensp@gmail.com>
2025-03-25 02:04:17 -0400

    Add past Codex challenges

---
+++ Variables/Variables.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Solve this riddle: I speak without a mouth and hear without ears." +
+        "I have no body, but I come alive with the wind. What am I?");
+        String answer = input.nextLine();
+
+        if (answer.equalsIgnoreCase("Echo")){
+            System.out.println("Wow. You found the correct answer, Congrats!");
+        } else {
+            System.out.println("Well, at least you tried... Kepp thinking.");
+        }
+
+
+        
+    }
+}
